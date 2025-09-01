8c532d7659d18f3ca06a1063013eca88bd854acb
erick <erick.krug@skysoftware.com.br>
2025-09-01 16:10:19 -0300

    commit no program

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.MapGet("/", () => "?? API rodando no Azure!");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
