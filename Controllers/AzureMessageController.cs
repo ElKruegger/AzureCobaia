51c2c9a8b4ba7c49257d298ac86b1f632ec5f83d
erick <erick.krug@skysoftware.com.br>
2025-09-01 16:54:44 -0300

    poggers CI/CD pelo github

--- Controllers/AzureMessageController.cs
+++ Controllers/AzureMessageController.cs
@@ -2,23 +2,30 @@
 
 namespace CobaiaAzure.Controllers
 {
-        [ApiController]
-        [Route("api/[controller]")]
+    [ApiController]
+    [Route("api/[controller]")]
     public class AzureMessageController : Controller
     {
-       
-            // GET: api/mensagem
-            [HttpGet]
-            public IActionResult GetMensagem()
-            {
-                return Ok(" .NET 8.0 as your service, i'm just an prototype, enjoy.");
-            }
 
-            // GET: api/mensagem/hello
-            [HttpGet("hello")]
-            public IActionResult GetHello()
-            {
-                return Ok(new { Mensagem = "Don't bother, im studying how destroy your world.." });
-            }
+        // GET: api/mensagem
+        [HttpGet("NetMessage")]
+        public IActionResult GetMensagem()
+        {
+            return Ok(" .NET 8.0 as your service, i'm just an prototype, enjoy.");
+        }
+
+        // GET: api/mensagem/hello
+        [HttpGet("DestroyMessage")]
+        public IActionResult GetHello()
+        {
+            return Ok("Don't bother, im studying how destroy your world..");
+        }
+
+
+        [HttpGet("Sybau")]
+        public IActionResult GetSybau()
+        {
+            return Ok("Ayo twin, shut yo bitch ass up u pmo sm >w< ");
+        }
     }
 }
