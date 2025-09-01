b3ec7dfdb471bd770deb5e6b18bb4ef8fe8aa327
erick <erick.krug@skysoftware.com.br>
2025-09-01 15:09:50 -0300

    Primeiro commit - API .NET 8

---
+++ Controllers/AzureMessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CobaiaAzure.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+    public class AzureMessageController : Controller
+    {
+       
+            // GET: api/mensagem
+            [HttpGet]
+            public IActionResult GetMensagem()
+            {
+                return Ok(" .NET 8.0 as your service, i'm just an prototype, enjoy.");
+            }
+
+            // GET: api/mensagem/hello
+            [HttpGet("hello")]
+            public IActionResult GetHello()
+            {
+                return Ok(new { Mensagem = "Don't bother, im studying how destroy your world." });
+            }
+    }
+}
